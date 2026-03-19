185be36856e72e0e83c4eaa72ffad722dc2ee6f5
Apurv <apurvd16@gmail.com>
2026-03-20 00:48:18 +0530

    Update store.ts

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -4,18 +4,30 @@ type overlayActive = {
   isActive: boolean;
   removeGif: boolean;
   isLandingReady: boolean;
+  preloaderProgress: number;
+  bitMapCache: ImageBitmap[][] | null;
   setActive: () => void;
   setRemoveGif: () => void;
   setLandingReady: (ready: boolean) => void;
+  setPreloaderProgress: (progress: number) => void;
+  setBitMapCache: (cache: ImageBitmap[][]) => void;
+  resetActive: () => void;
+  resetRemoveGif: () => void;
 };
 
 const useOverlayStore = create<overlayActive>((set) => ({
   isActive: false,
   removeGif: false,
   isLandingReady: false,
+  preloaderProgress: 0,
+  bitMapCache: null,
   setActive: () => set({ isActive: true }),
   setRemoveGif: () => set({ removeGif: true }),
   setLandingReady: (ready) => set({ isLandingReady: ready }),
+  setPreloaderProgress: (progress) => set({ preloaderProgress: progress }),
+  setBitMapCache: (cache) => set({ bitMapCache: cache }),
+  resetActive: () => set({ isActive: false }),
+  resetRemoveGif: () => set({ removeGif: false }),
 }));
 
 export default useOverlayStore;
